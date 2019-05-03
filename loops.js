0203086ab62696545ef2c5b3b99b94ec14f3236c
Marco Frisina <marco@frisina.xyz>
2019-05-03 21:55:26 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -4,7 +4,7 @@ array[i] = `I am ${i} strange loops.`;
 }
 if (array[1] === `I am 1 strange loops.`) {
 array[1] = `I am 1 strange loop.`;
-} else if (array[11] === `I am 1 strange loops.`) {
+} else if (array[11] === `I am 11 strange loops.`) {
   array[11] = `I am 1 strange loop.`;
 }
 return array;
