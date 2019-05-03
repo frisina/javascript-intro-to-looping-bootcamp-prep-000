39c82b924af518aebdcfb8895544a2a36c4296b0
Marco Frisina <marco@frisina.xyz>
2019-05-03 23:08:26 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -27,6 +27,6 @@ function incrementVariable() {
 
 function doWhileLoop(num) {
   do {
-    console.log("doo-bee-doo-bee-doo");
+    console.log("I run regardless.");
   } while (incrementVariable() < num);
 }
