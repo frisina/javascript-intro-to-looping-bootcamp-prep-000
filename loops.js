93cc4c77dc62b8684bb3754070d1b294bedb8cc2
Marco Frisina <marco@frisina.xyz>
2019-05-03 21:15:24 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -2,8 +2,8 @@ function forLoop(array) {
  for(let i = 0; i < 26; i++) {
 	array[i] = `I am ${i} strange loops.`;
    }
-   if (array[i[ = 1]) {
-     array[i] = 'I am 1 strange loop.';
+   if (array[i] = 1) {
+     array[i] = `I am 1 strange loop.`;
    }
 
 return array;  }
