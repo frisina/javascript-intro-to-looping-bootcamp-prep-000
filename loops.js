3dbe1515f46c2522e36d7fbcec56ac5250eb2ea1
Marco Frisina <marco@frisina.xyz>
2019-05-03 21:11:07 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -3,6 +3,6 @@ function forLoop(array) {
 	array[i] = `I am ${i} strange loops.`;
    }
 	array[0] = 'I am 1 strange loop.';
-  array[11] = 'I am 1 strange loop';
+  array[11] = 'I am 1 strange loop.';
 
 return array;  }
